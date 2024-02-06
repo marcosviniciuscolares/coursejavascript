3e0ec5a527416f39215ea595bf9cdac652f10d0b
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2024-02-06 14:19:57 -0300

    .

--- ex2/logic.js
+++ ex2/logic.js
@@ -1,1 +1,6 @@
+let username;
 
+document.getElementById('mybutton').onclick = function(){
+    username = document.getElementById("itext").value;
+    document.getElementById('myh1').textContent = `Hello, ${username}`;
+}
