bea7093ea4cd552c09ad80d7d20d3541f4d3bc78
Avantiermv <marcosviniciussilvacolares@gmail.com>
2024-11-15 14:38:02 -0300

    Aulas do curso.

---
+++ section2/aula5/index.js
@@ -0,0 +1,11 @@
+const nome = 'joão';
+console.log(nome);
+
+const firstNumber = 5;
+const secondNumber = 10;
+const calculus = firstNumber * secondNumber;
+console.log(calculus);
+
+// typeof identifica o tipo da variável.
+
+console.log(typeof (calculus));
---
+++ section2/aula6/index.js
@@ -0,0 +1,15 @@
+const name = 'Marcos vincius';
+const lastName = 'Silva colares';
+const age = 19;
+const weight = 46;
+const height = 1.72;
+let imc;
+let birthYear;
+
+ imc = weight / (height * height);
+ birthYear = 2024 - age;
+
+ console.log(`${name} ${lastName} is ${age} years old, his birth year is in ${birthYear} and his imc is ${imc}.`);
+
+
+
