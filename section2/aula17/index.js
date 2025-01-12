4fbc48747a5554ee009ea02b7d5eb680733a8589
Marcos Vinicius <marcosviniciussilvacolares@gmail.com>
2025-01-12 08:54:26 -0300

    .

---
+++ section2/aula17/index.js
@@ -0,0 +1,6 @@
+let names = ['Marcos', 'maria', 'joão'];
+names = 123;
+
+console.log(typeof names);
+console.log(names instanceof Array);
+console.log(names);
---
+++ section2/aula18/index.js
@@ -0,0 +1,12 @@
+function divisão(x, y){
+    const resultado = x / y;
+    return resultado;
+}
+
+console.log(divisão(10,2));
+console.log(divisão(12, 3));
+
+
+const raiz = n => n**0.5;
+
+console.log(raiz(9));
