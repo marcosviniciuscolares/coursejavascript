f7295bb7e6ba34f6beb09fab2be5a7edc78593b3
Marcos Vinicius <marcosviniciussilvacolares@gmail.com>
2025-03-03 13:41:25 -0300

    .

---
+++ section3/ex9/index.js
@@ -0,0 +1,14 @@
+function adicionarTarefa(){
+
+    let tarefa = document.querySelector('#tarefa');
+    let tarefaAdicionada;
+    const adicionar = document.querySelector('#button-submit');
+
+    adicionar.addEventListener('click', () => {
+       
+    });
+
+    
+
+}
+adicionarTarefa();
